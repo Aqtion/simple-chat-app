2dbfe38197bcf4e04b8a0af812484de7b714c24c
KevinTvT <kevin.nie.24@sacredsf.org>
2023-07-24 14:56:31 -0400

    Added message class

---
+++ message.py
@@ -0,0 +1,18 @@
+from datetime import datetime, date
+import client
+
+class message:
+    '''Represents a message and contains the metadata for each message
+    metadata: 
+        Date message was sent in the format yr/month/day
+        Time message was sent
+        User that sent message
+        Message that was sent'''
+    def __init__(self, sender: client, msg: str):
+        self.sendDate = date.today()  #date.today() returns in the format yr/mon/day
+        self.sendTime = datetime.now().strftime("%H:%M:%S")
+        self.sender = sender
+        self.msg = msg
+    
+    if __name__ == "__main__":
+        pass
