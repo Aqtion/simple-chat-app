7dbd44b4350ede5fa360475f376bbe7340d64ce0
kevin <kunyewu06@gmail.com>
2023-07-24 15:08:33 -0400

    server stuff

--- chat_server.py
+++ chat_server.py
@@ -1,1 +1,26 @@
-# init
+import socket
+import server
+
+# Global veriables
+PORT = 8000
+ADDRESS = ("", PORT)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(ADDRESS)
+CLIENTS = {}
+
+serv = server.Server()
+
+
+def broadcast_message(data, conn, sender):
+    for client in CLIENTS:
+        if client.ip_address != sender:
+            conn.send("<" + {client.username} + "> " + data.encode())
+
+
+while True:
+    conn, addr = s.accept()
+    if not CLIENTS[addr]:
+        broadcast_message("has joined!", conn, addr)
+        CLIENTS[addr] = conn
+    data = conn.recv(1024)
+    broadcast_message(data, conn, addr)
