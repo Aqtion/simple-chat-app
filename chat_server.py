05ba32d678ef111d09bf1960a6af7d10bd4d78f7
Suhruth Vuppala <svuppala1@wpi.edu>
2023-07-25 09:07:23 -0400

    Chat server works

--- chat_server.py
+++ chat_server.py
@@ -5,7 +5,7 @@ import client
 import message
 
 # Global veriables
-serv = server.SERVER("10.29.60.96", 8001)
+serv = server.Server("10.29.60.96", 8001)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,8 +18,8 @@ def broadcast_message(data, addr):
         if client != addr[0]:
             connection = CLIENTS[client][0]
             user = CLIENTS[client][1]
-            message = message.Message(user, data)
-            connection.send(message.create_message())
+            mess = message.Message(user, data)
+            connection.send(mess.create_message())
 
 
 def server(conn, addr):
@@ -27,7 +27,7 @@ def server(conn, addr):
     # broadcast_message("has joined!", conn, addr)
     while True:
         data = conn.recv(1024).decode()
-        if data.substr(0, 8) == "USERINFO":
+        if data[0:8] == "USERINFO":
             user_info = data.split(" ")
             user = client.Client(user_info[1], str(addr[0]), user_info[3], user_info[2])
             CLIENTS[addr[0]].append(user)
